000bd1d646b34778af5eecb5abd1bcfe9b25a95a
Xuan Thang Nguyen <nguyexu2@fit.cvut.cz>
2021-04-13 17:23:00 +0200

    multi thread

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
+    private static final ExecutorService threadPool = Executors.newCachedThreadPool();
     public static void main(String[] args) {
         System.out.println("starting\n");
         int port = 3999;
@@ -17,8 +20,14 @@ public class Main {
         }
 
         while (true) {
-            try (Socket clientSocket = serverSocket.accept()) {
-                new Server(clientSocket).run();
+            Socket clientSocket;
+            try  {
+                clientSocket = serverSocket.accept();
+                long threadId = Thread.currentThread().getId();
+                System.out.println("main using " + threadId);
+                Server runnable = new Server(clientSocket);
+                Thread thread = new Thread(runnable);
+                thread.start();
             } catch (IOException e) {
                 System.out.println("Couldn't create connection to port " + port);
                 return;
--- src/Server.java
+++ src/Server.java
@@ -2,8 +2,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
@@ -33,17 +31,94 @@ public class Server implements Runnable {
     static final String CLIENT_FULL_POWER = "FULL POWER";
     static final String SERVER_LOGIC_ERROR = "302 LOGIC ERROR";
 
-    static final int TIMEOUT = 800; //1second
+    static final int TIMEOUT = 1000; //1second
     static final int TIMEOUT_RECHARGING = 5000;//5seconds
 
     //-----------------------------------
 
     public Server(Socket clientSocket) throws IOException {
         this.clientSocket = clientSocket;
-        this.clientSocket.setSoTimeout(TIMEOUT);
         this.in = new Scanner(this.clientSocket.getInputStream()).useDelimiter(delim);
         this.out = new PrintWriter(this.clientSocket.getOutputStream(), true);
         this.keys = new Keys();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("new client connected...");
+        long threadId = Thread.currentThread().getId();
+        System.out.println("server using " + threadId);
+
+        try {
+            if (!authenticate()) {
+                System.out.println("authentication failed, ending connection...");
+                cleanup();
+                return;
+            }
+        } catch (TimeoutException e) {
+            System.out.println("timed out, ending connection...");
+            cleanup();
+            return;
+        } catch (SocketException e) {
+            e.printStackTrace();
+            cleanup();
+            return;
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            send(SERVER_LOGIC_ERROR);
+            System.out.println("logic error, ending connection...");
+            cleanup();
+            return;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            send(SERVER_SYNTAX_ERROR);
+            System.out.println("syntax error, ending connection...");
+            cleanup();
+            return;
+        }
+
+
+        RobotController controller = new RobotController(this::receive, this::send);
+
+        try {
+            controller.run();
+
+        } catch (TimeoutException e) {
+            System.out.println("timed out, ending connection...");
+            cleanup();
+            return;
+        } catch (SocketException e) {
+            e.printStackTrace();
+            cleanup();
+            return;
+        } catch (
+                IllegalStateException e) {
+            e.printStackTrace();
+            send(SERVER_LOGIC_ERROR);
+            System.out.println("logic error, ending connection...");
+            cleanup();
+            return;
+        } catch (
+                IllegalArgumentException e) {
+            e.printStackTrace();
+            send(SERVER_SYNTAX_ERROR);
+            System.out.println("syntax error, ending connection...");
+            cleanup();
+            return;
+        }
+
+        send(SERVER_LOGOUT);
+        cleanup();
+    }
+
+    private void cleanup() {
+        try {
+            clientSocket.close();
+            in.close();
+            out.close();
+        } catch (Exception e) {
+            System.out.println("Can not close: " + e);
+        }
     }
 
     public static String toPrintable(String in) {
@@ -62,7 +137,9 @@ public class Server implements Runnable {
     }
 
     private String receive(int maxLength) throws TimeoutException, IllegalStateException, SocketException {
-        userInput = read(maxLength);
+        this.clientSocket.setSoTimeout(TIMEOUT);
+
+        userInput = read(Math.max(maxLength, CLIENT_RECHARGING.length()));
 
         if (userInput.equals(CLIENT_RECHARGING)) {
             this.clientSocket.setSoTimeout(TIMEOUT_RECHARGING);
@@ -74,18 +151,26 @@ public class Server implements Runnable {
             userInput = read(maxLength);
         }
 
+        if (maxLength != -1 && userInput.length() > maxLength)
+            throw new IllegalArgumentException("input too long");
+
         return userInput;
     }
 
     private String read(int maxLength) throws TimeoutException, IllegalArgumentException {
         try {
+
             String ret = "";
             boolean breakout = false;
 
             while (!breakout) {
-                breakout = in.hasNext() && !in.hasNext(".*\\z");
+                breakout = (in.hasNext() && !in.hasNext(".*\\z"));
 
                 ret += in.next();
+
+                if (!breakout)
+                    System.out.println("received incomplete --> |" + toPrintable(ret) + "|");
+
                 if (maxLength != -1 && ret.length() > maxLength)
                     throw new IllegalArgumentException("input too long");
             }
@@ -100,61 +185,13 @@ public class Server implements Runnable {
         }
     }
 
-    @Override
-    public void run() {
-        System.out.println("new client connected...");
-
-        try {
-            if (!authenticate()) {
-                System.out.println("authentication failed, ending connection...");
-                return;
-            }
-        } catch (TimeoutException | SocketException e) {
-            System.out.println("timed out, ending connection...");
-            return;
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            send(SERVER_LOGIC_ERROR);
-            System.out.println("logic error, ending connection...");
-            return;
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            send(SERVER_SYNTAX_ERROR);
-            System.out.println("syntax error, ending connection...");
-            return;
-        }
-
-
-        RobotController controller = new RobotController(this::receive, this::send);
-
-        try {
-            controller.run();
-
-        } catch (TimeoutException | SocketException e) {
-            System.out.println("timed out, ending connection...");
-            return;
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-            send(SERVER_LOGIC_ERROR);
-            System.out.println("logic error, ending connection...");
-            return;
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-            send(SERVER_SYNTAX_ERROR);
-            System.out.println("syntax error, ending connection...");
-            return;
-        }
-
-        send(SERVER_LOGOUT);
-    }
-
     private Boolean authenticate() throws TimeoutException, IllegalStateException, SocketException, IllegalArgumentException {
         receive(18);
         this.username = userInput;
 
         send(SERVER_KEY_REQUEST);
 
-        receive();
+        receive(3);
         int ID = Integer.parseInt(userInput);
         if (ID >= keys.getSize() || ID < 0) {
             send(SERVER_KEY_OUT_OF_RANGE_ERROR);
